e6e8a294816b518d4c71cbf0f3a2c46e22c57830
mayf <cjm2021401@khu.ac.kr>
2021-11-03 22:08:36 +0900

    first commit

---
+++ src/main/java/com/example/demo/Controller/ct.java
@@ -0,0 +1,10 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ct {
+    @GetMapping("/")
+    public String home() { return "home"; }
+}
